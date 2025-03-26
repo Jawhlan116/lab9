046200038c645a5f07bf597b7ce9a192b12decad
Jawhlan116 <jawkhlan2005@gmail.com>
2025-03-26 12:41:46 +0800

    assert-iig anh uda ashiglasan jishee

---
+++ assertionjish1/assertionsjish1.java
@@ -0,0 +1,14 @@
+package jishe1;
+import java.util.*;
+
+public class assertionsjish1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		assert x>=0;
+		sc.close();
+	}
+
+}
