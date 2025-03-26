9fec3d2e86a9d2627ad720d6393567acf5a6091b
Jawhlan116 <jawkhlan2005@gmail.com>
2025-03-26 12:42:23 +0800

    assert aldaa messagiig oorchilson jishee

---
+++ assertionjish2/assertionjish2.java
@@ -0,0 +1,15 @@
+package jishe1;
+import java.util.*;
+
+public class assertionjish2 {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		assert x>=0: "x<0 өөрөөр хэлбэл х-ийн утга 0-ээс бага байна!";
+		sc.close();
+
+	}
+
+}
